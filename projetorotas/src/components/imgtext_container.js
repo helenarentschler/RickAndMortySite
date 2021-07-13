524685953a8ab241d2a01f4c687c2e0992e1cbb2
Helena Rentschler <helrentschler@gmail.com>
2021-07-13 15:16:33 -0300

    create css's containerImage--text and card--medium

--- projetorotas/src/components/imgtext_container.js
+++ projetorotas/src/components/imgtext_container.js
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 export default class ImgTextContainer extends Component{
     render(){
         return(
-            <div className="card">
+            <div className="card card--medium">
                 <img className="containerImage containerImage--text" src={this.props.link}/>
                 <p>
                     Status: <br />
